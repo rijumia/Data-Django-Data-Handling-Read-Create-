c9eeb6e9ed6c3989b85f8c5bb495f071bce2f3a3
rijumia <riju.mia2612@gmail.com>
2025-05-30 17:25:41 +0600

    Create Student Model

--- studentApp/models.py
+++ studentApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class studentModel(models.Model):
+    StudentName = models.CharField(max_length=100)
+    StudentAddress = models.TextField(max_length=200)
+    StudentDepartment = models.CharField(max_length=100)
+    StudentAge = models.IntegerField()
